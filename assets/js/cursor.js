1b7c11c30769bb4d5adbfd2c593c7e92a85d4145
dryhour <derekp9210@gmail.com>
2025-11-18 19:52:18 -0500

    fix: cursor

--- assets/js/cursor.js
+++ assets/js/cursor.js
@@ -1,33 +1,12 @@
-const coords = { x: 0, y: 0};
+const coords = { x: 0, y: 0 };
 const circles = document.querySelectorAll(".circle");
 
-const colors = [
-    '#000000',
-    '#050000',
-    '#0a0000',
-    '#0e0000',
-    '#120811',
-    '#161024',
-    '#1a1737',
-    '#1e1f4a',
-    '#22265d',
-    '#262d70',
-    '#2a3483',
-    '#2e3b96',
-    '#3243a9',
-    '#364abc',
-    '#3a52cf',
-    '#3e59e2',
-    '#4260f5'
-];
-
-circles.forEach(function(circle, i){
+circles.forEach(circle => {
     circle.x = 0;
     circle.y = 0;
-    circle.style.backgroundColor = colors[i % colors.length];
 });
 
-window.addEventListener("mousemove", function(e){
+window.addEventListener("mousemove", e => {
     coords.x = e.clientX;
     coords.y = e.clientY;
 });
@@ -36,21 +15,19 @@ function animateCircles(){
     let x = coords.x;
     let y = coords.y;
 
-    circles.forEach(function(circle, index){
+    circles.forEach((circle, index) => {
         circle.style.left = x - 12 + "px";
         circle.style.top = y - 12 + "px";
-
-        circle.style.scale = (circles.length - index) / circles.length;
-
+        circle.style.transform = `scale(${1 - index * 0.01})`;
         circle.x = x;
         circle.y = y;
 
         const nextCircle = circles[index + 1] || circles[0];
-        x += (nextCircle.x - x) * 0.3;
-        y += (nextCircle.y - y) * 0.3;
+        x += (nextCircle.x - x) * 0.1;
+        y += (nextCircle.y - y) * 0.1;
     });
 
     requestAnimationFrame(animateCircles);
-};
+}
 
 animateCircles();
